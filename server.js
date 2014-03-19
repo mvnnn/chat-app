a5c03a6575a9b84528df953d9c80d5297a204fb2
App Academy Student <student@appacademy.io>
2014-03-18 18:04:14 -0700

    update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         file.serve(request, response);
     }).resume();
-}).listen(80);
+}).listen(process.env.PORT || 8080);
 
 chat(app)
 
